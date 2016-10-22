797aff946ab38d8e857d65ef2904c66cb905dbb1
egoitzbarrena <egoitzbarrena@ymail.com>
2016-10-22 20:10:43 +0200

    commit

---
+++ src/routes.js
@@ -0,0 +1,47 @@
+(function () {
+'use strict';
+
+angular.module('MenuApp')
+.config(RoutesConfig);
+
+RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+function RoutesConfig($stateProvider, $urlRouterProvider) {
+
+  // Redirect to home page if no other URL matches
+  $urlRouterProvider.otherwise('/home');
+
+  // *** Set up UI states ***
+  $stateProvider
+
+
+  .state('home', {
+    url: '/home',
+    templateUrl: 'src/menuapp/templates/home.template.html'
+  })
+
+  .state('categories', {
+    url: '/categories',
+    templateUrl: 'src/menuapp/templates/categories.template.html',
+    controller: 'CategoriesController as cate',
+    resolve: {
+      categories: ['MenuDataService', function(MenuDataService) {
+        return MenuDataService.getAllCategories();
+      }]
+    }
+  })
+
+  .state('items', {
+    url: '/items/{categoryShortName}',
+    templateUrl: 'src/menuapp/templates/items.template.html',
+    controller: "ItemsController as it",
+    resolve: {
+      items: ['$stateParams', 'MenuDataService', function ($stateParams, MenuDataService) {
+        return MenuDataService.getItemsForCategory($stateParams.categoryShortName);
+      }]
+    }
+  });
+
+
+}
+
+})();
